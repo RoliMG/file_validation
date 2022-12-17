6ab8fa2c310c29e8a4c64dafb18510c66b5073e0
RoliMG <rolandkretschmer4@gmail.com>
2022-12-17 15:01:07 +0100

    Add files via upload

---
+++ validate_directories.py
@@ -0,0 +1,43 @@
+import glob
+import hashlib
+import os
+
+def md5(fname):
+    hash_md5 = hashlib.md5()
+    with open(fname, "rb") as f:
+        for chunk in iter(lambda: f.read(4096), b""):
+            hash_md5.update(chunk)
+    return hash_md5.hexdigest()
+
+def get_files(dir):
+    files = []
+    for filename in glob.iglob(dir + '**/**', recursive=True):
+        if os.path.isfile(filename):
+            files.append(filename)
+        
+    return files
+
+dir1 = "W:/"
+dir2 = "X:/"
+log_dir = "W:/mismatch.txt"
+
+print(f"Getting files from {dir1}")
+files = get_files(dir1)
+i = 0
+
+print("Comparing hashes")
+for f in files:
+    if i % 100 == 0:
+        print(f"{i}/{len(files)}")
+
+    f_md5 = md5(f)
+    f2_md5 = md5(f"{dir2[0]}{f[1:]}")
+    
+    if f_md5 != f2_md5:
+        with open(log_dir, "a") as log_f:
+            msg = f"Hash mismatch {dir2[0]}{f[1:]}"
+            print(msg)
+            log_f.write(msg)
+            
+    i+=1
+
