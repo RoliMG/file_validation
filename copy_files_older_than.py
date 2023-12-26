95a5242c90d2c65933277a33c2ffda9a4a031f3a
Roli <rolandkretschmer4@gmail.com>
2023-12-26 12:16:29 +0100

    added exif extraction

--- copy_files_older_than.py
+++ copy_files_older_than.py
@@ -1,11 +1,44 @@
 import logging
+from datetime import datetime
+from time import mktime
 
 import progressbar
+from exif import Image
 from progressbar import ProgressBar
 
 from util import get_files
 import os
 import shutil
+
+
+def is_video(file):
+    types = {'webm', 'mkv', 'flv', 'vob', 'ogv', 'ogg', 'rrc', 'gifv', 'mng', 'mov', 'avi', 'qt', 'wmv', 'yuv', 'rm',
+             'asf', 'amv', 'mp4', 'm4p', 'm4v', 'mpg', 'mp2', 'mpeg', 'mpe', 'mpv', 'm4v', 'svi', '3gp', '3g2', 'mxf',
+             'roq', 'nsv', 'flv', 'f4v', 'f4p', 'f4a', 'f4b', 'mod'}
+
+    _, ext = os.path.splitext(file)
+    return ext[1:] in types
+
+
+def is_image(file):
+    types = {'.heic', '.ras', '.xwd', '.bmp', '.jpe', '.jpg', '.jpeg', '.xpm', '.ief', '.pbm', '.tif', '.gif', '.ppm',
+             '.xbm', '.tiff', '.rgb', '.pgm', '.png', '.pnm'}
+    _, ext = os.path.splitext(file)
+    return ext in types
+
+
+def get_time(file: str) -> int:
+    with open(file, 'rb') as image_file:
+        try:
+            img = Image(image_file)
+        except:
+            return int(os.path.getmtime(file))
+
+    if not is_image(file) or not is_video(file) or not img.has_exif or 'datetime' not in img.list_all():
+        return int(os.path.getmtime(file))
+
+    return int(mktime(datetime.strptime(img.datetime, "%Y:%m:%d %H:%M:%S").timetuple()))
+
 
 progressbar.streams.wrap_stderr()
 logging.basicConfig(encoding='utf-8',
@@ -17,18 +50,19 @@ logging.basicConfig(encoding='utf-8',
                     ])
 
 time = 1671205474
-src = "Y:/Photos"
+src = "Y:/Photos/DCIM"
 dest = "D:/nas_bilder"
 
 logging.info("Scanning files")
 files, _ = get_files(src)
 dates: list[tuple[int, str]] = []
 
-for file in files:
-    dates.append((int(os.path.getmtime(file)), file))
+logging.info("Retrieving timestamp from files")
+
+for f in files:
+    dates.append((get_time(f), f))
 
 most_recent = [f[1] for f in dates if f[0] > time]
-
 
 pbar = ProgressBar(max_value=len(most_recent))
 
--- test.py
+++ test.py
@@ -1,11 +1,18 @@
-import argparse
+import os
 import time
+from datetime import datetime
 
-import progressbar
-from progressbar import ProgressBar
+from exif import Image
 
-pbar = ProgressBar(max_value=100)
 
-for i in range(100):
-    pbar.update(i)
-    time.sleep(1)
+def get_date(file: str) -> int:
+    with open(file, 'rb') as image_file:
+        img = Image(image_file)
+
+    if not img.has_exif or 'datetime' not in set(img.list_all()):
+        return int(os.path.getmtime(file))
+
+    return int(time.mktime(datetime.strptime(img.datetime, "%Y:%m:%d %H:%M:%S").timetuple()))
+
+
+print(get_date("pic2.jpg"))
